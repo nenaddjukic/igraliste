55e85f1d770b9fce34429304b28b77b191b404b9
enendju <nenad.djukic@ericsson.com>
2013-03-25 12:21:40 +0000

    small changes

--- igraliste-jca-jar/src/main/java/com/igraliste/jca/camel/CamelEngine.java
+++ igraliste-jca-jar/src/main/java/com/igraliste/jca/camel/CamelEngine.java
@@ -1,6 +1,7 @@
 package com.igraliste.jca.camel;
 
 import javax.jms.ConnectionFactory;
+import javax.naming.InitialContext;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
@@ -20,22 +21,24 @@ public class CamelEngine {
 
 	public void startFileTransfering(String fileName) {
 		log.debug("Received work to transfer: {}", fileName);
-		CamelContext context = new DefaultCamelContext();
-		ConnectionFactory connectionFactory = new HornetQJMSConnectionFactory();
-		context.addComponent("jms",
-				JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
 		try {
+			CamelContext context = new DefaultCamelContext();
+			InitialContext ic = new InitialContext();
+			ConnectionFactory connectionFactory = (ConnectionFactory) ic
+					.lookup("/ConnectionFactory");
+			context.addComponent("jms",
+					JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
 			context.addRoutes(new RouteBuilder() {
 				public void configure() {
 					from(
-							"ftp://rider.com/orders"
-									+ "?username=rider&password=secret").to(
+							"ftp://byethost22.com/ftp"
+									+ "?username=b22_12664855&password=sheckey7").to(
 							"jms:IncomingMessages");
 				}
 			});
 			context.start();
 		} catch (Exception e) {
-			log.error("ERROR IN creating camel rout: {}",e);
+			log.error("ERROR IN creating camel rout: {}", e);
 		}
 	}
 
