82d35e02fb1794fd25c623dae7d3d7f7595a4115
enendju <nenad.djukic@ericsson.com>
2013-06-10 16:43:00 +0100

    store message to cache and retrieve it on message.

--- igraliste-jar/src/main/java/com/igraliste/ejb/CamelEnricherImpl.java
+++ igraliste-jar/src/main/java/com/igraliste/ejb/CamelEnricherImpl.java
@@ -2,21 +2,30 @@ package com.igraliste.ejb;
 
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.igraliste.api.CacheEntity;
 import com.igraliste.api.CamelEnricher;
+import com.igraliste.ejb.util.cache.CacheCreator;
 
 @Stateless
 @Remote(CamelEnricher.class)
 public class CamelEnricherImpl implements CamelEnricher{
 
 	Logger log = LoggerFactory.getLogger(CamelEnricherImpl.class);
+	
+	@Inject
+	private CacheCreator creator;
+	
 	@Override
 	public String storeCamelMessage(String message) {
 		log.debug("Storing Camel message to cache: {}",message);
-		return message;		
+		CacheEntity entry = new CacheEntity("2", message);
+		creator.put("2", entry);
+		return "2";		
 	}
 
 }
--- igraliste-jar/src/main/java/com/igraliste/ejb/IncomingMessagesObserver.java
+++ igraliste-jar/src/main/java/com/igraliste/ejb/IncomingMessagesObserver.java
@@ -2,6 +2,7 @@ package com.igraliste.ejb;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.inject.Inject;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -9,6 +10,9 @@ import javax.jms.MessageListener;
 import org.hornetq.jms.client.HornetQTextMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.igraliste.api.CacheEntity;
+import com.igraliste.ejb.util.cache.CacheCreator;
 
 @MessageDriven(name = "MessageMDBSample", activationConfig = {
 		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
@@ -18,15 +22,18 @@ public class IncomingMessagesObserver implements MessageListener {
 
 	private Logger log = LoggerFactory
 			.getLogger(IncomingMessagesObserver.class);
+	@Inject
+	private CacheCreator creator;
 
-	public void onMessage(Message message) {
-		log.debug("IncomingMessagesObserver has received the message");
-		HornetQTextMessage tm = (HornetQTextMessage) message;
-		//String content = processByteMessage(tm);
+	public void onMessage(final Message message) {
+		log.debug("IncomingMessagesObserver has received the message");		
 		try {
-			log.debug("Received message " + tm.getText());
+			HornetQTextMessage tm = (HornetQTextMessage) message;
+			final String result = tm.getText();
+			log.debug("Received message " + result);
+			final CacheEntity res = creator.get(result);
+			log.debug("Result is: {}",res.getValue());
 		} catch (JMSException e) {
-			// TODO Auto-generated catch block
 			log.error("There was an error in JMS handler: {}",e);
 		}
 	}
